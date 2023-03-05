49f9edaa21444435c9e215e904b2130eea2ea635
Dmytro2V <op2vin@gmail.com>
2023-03-05 16:49:52 +0200

    done

--- sql-practice/seed-data.sql
+++ sql-practice/seed-data.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS puppies;
 
+.print ---------- CREATING TABLE puppies ----------;
 CREATE TABLE puppies (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(100),
@@ -21,3 +22,22 @@ VALUES
   (8, 'Jaxson', 0.4, 'Beagle', 19, 1),
   (9, 'Leinni', 1, 'Miniature Schnauzer', 25, 1),
   (10, 'Max', 1.6, 'German Shepherd', 65, 0);
+
+.print --------   Step 1: Select all rows, all columns;
+SELECT * FROM puppies;
+
+.print --------   Step 2: Select specific columns;
+SELECT name, age_yrs, weight_lbs FROM puppies;
+
+.print --------   Step 3: Select one row by its primary key;
+SELECT name, age_yrs, weight_lbs FROM puppies WHERE id = 5;
+
+.print --------   Step 4: Select rows matching a specific value;
+SELECT name, age_yrs, weight_lbs FROM puppies WHERE microchipped = TRUE;
+-- checked : also works with true, and with 1
+
+.print --------   Step 5 INTERMEDIATE: Select rows greater than a value;
+SELECT name, age_yrs, weight_lbs FROM puppies WHERE weight_lbs > 25;
+
+.print --------   Step 6 ADVANCED: Select rows with multiple criteria;
+SELECT name, age_yrs, weight_lbs FROM puppies WHERE weight_lbs > 25 AND microchipped = true;
